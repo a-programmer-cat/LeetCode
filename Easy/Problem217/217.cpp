12911f9539acc144e24884b8cdae3697de8378b7
一只程序猫 <143919677+a-programmer-cat@users.noreply.github.com>
2024-01-13 08:41:23 +0800

    Create 217.cpp

---
+++ Easy/Problem217/217.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for (int i = 0 ; i < nums.size() ; ++i){
+            if (i != nums.size() - 1){
+                if (nums[i] == nums[i+1]){
+                    return true;
+                }
+            } 
+        }
+        return false;
+    }
+};
