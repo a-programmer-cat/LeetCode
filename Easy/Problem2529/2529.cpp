5668189f5e9cf304752c49c3a64adee033179b9c
一只程序猫 <143919677+a-programmer-cat@users.noreply.github.com>
2024-04-09 07:29:26 +0800

    Create 2529.cpp

---
+++ Easy/Problem2529/2529.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumCount(vector<int>& nums) {
+        int pos = 0;
+        int neg = 0;
+        for(int i = 0; i<nums.size(); i++){
+            if (nums[i] != 0){
+                if (nums[i] < 0){
+                    neg++;
+                }else{
+                    pos++;
+                }
+            }
+        }
+        return pos>neg?pos:neg;
+    }
+};
